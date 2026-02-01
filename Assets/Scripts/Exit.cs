9b54538352e421c89a639eef7a69f45b797347f8
Jon Deaves <hello@jondeaves.me>
2026-02-01 11:03:01 -0500

    add exit trigger to win

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+		{
+			GameManager.Instance.PlayerWon = true;
+			SceneManager.LoadScene("GameOverScene");
+		}
+    }
+}
